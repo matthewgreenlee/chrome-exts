90a1d6ef6cc5a00ce79e888aa19edf0be575ea14
matthewgreenlee <matthew.green.lee@gmail.com>
2012-04-25 16:30:53 +0800

    increment version

--- HistoryDig/popup.js
+++ HistoryDig/popup.js
@@ -4,6 +4,17 @@ chrome.tabs.getSelected(null, function(tab) {
 
 function doPopup(url) {
   chrome.history.getVisits({'url': url}, function(visits) {
-    document.write(visits.length + " visits to " + url);
+    document.writeln("Total " + visits.length + " visits to " + url);
+	var transitionTypes = ["link", "typed", "auto_bookmark", "auto_subframe", "manual_subframe", "generated", "start_page", "form_submit", "reload", "keyword", "keyword_generated"];
+	for(var i in transitionTypes) {
+	  document.writeln(filterWithTransition(transitionTypes[i], visits).length + " " + transitionTypes[i] + " visits");
+	}
   });
+}
+
+function filterWithTransition(transitionType, visitItems) {
+  var filteredItems = visitItems.filter(function(item, index, array) {
+    return item.transition === transitionType;
+  });
+  return filteredItems;
 }
