02c76d71272483dfb83d468303c9d4aec718ad7e
matthewgreenlee <matthew.green.lee@gmail.com>
2012-04-26 14:59:51 +0800

    add options page and use localStorage

--- HistoryDig/popup.js
+++ HistoryDig/popup.js
@@ -4,10 +4,11 @@ chrome.tabs.getSelected(null, function(tab) {
 
 function doPopup(url) {
   chrome.history.getVisits({'url': url}, function(visits) {
-    document.writeln("Total " + visits.length + " visits to " + url);
+    document.writeln("<div>Total " + visits.length + " visits to " + url + "</div>");
 	var transitionTypes = ["link", "typed", "auto_bookmark", "auto_subframe", "manual_subframe", "generated", "start_page", "form_submit", "reload", "keyword", "keyword_generated"];
-	for(var i in transitionTypes) {
-	  document.writeln(filterWithTransition(transitionTypes[i], visits).length + " " + transitionTypes[i] + " visits");
+	for(var i = 0; i < transitionTypes.length; i++) {
+	  if (localStorage[transitionTypes[i]] === "true")
+	    document.writeln("<div>" + filterWithTransition(transitionTypes[i], visits).length + " " + transitionTypes[i] + " visits</div>");
 	}
   });
 }
