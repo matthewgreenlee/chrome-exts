d3a94f149f227dec4a6d3c75cee49c7568e3699a
matthewgreenlee <matthew.green.lee@gmail.com>
2012-04-27 17:43:27 +0800

    add timespan radio in options page

--- HistoryDig/popup.js
+++ HistoryDig/popup.js
@@ -4,11 +4,12 @@ chrome.tabs.getSelected(null, function(tab) {
 
 function doPopup(url) {
   chrome.history.getVisits({'url': url}, function(visits) {
-    document.writeln("<div>Total " + visits.length + " visits to " + url + "</div>");
+    var timespanVisits = filterWithVisitTime(localStorage["timespan"], visits);
+    document.writeln("<div>Within last " + localStorage["timespan"] + " days total " + timespanVisits.length + " visits to " + url + "</div>");
 	var transitionTypes = ["link", "typed", "auto_bookmark", "auto_subframe", "manual_subframe", "generated", "start_page", "form_submit", "reload", "keyword", "keyword_generated"];
 	for(var i = 0; i < transitionTypes.length; i++) {
 	  if (localStorage[transitionTypes[i]] === "true")
-	    document.writeln("<div>" + filterWithTransition(transitionTypes[i], visits).length + " " + transitionTypes[i] + " visits</div>");
+	    document.writeln("<div>" + filterWithTransition(transitionTypes[i], timespanVisits).length + " " + transitionTypes[i] + " visits</div>");
 	}
   });
 }
@@ -16,6 +17,13 @@ function doPopup(url) {
 function filterWithTransition(transitionType, visitItems) {
   var filteredItems = visitItems.filter(function(item, index, array) {
     return item.transition === transitionType;
+  });
+  return filteredItems;
+}
+
+function filterWithVisitTime(timespan, visitItems) {
+  var filteredItems = visitItems.filter(function(item, index, array) {
+    return (new Date().getTime() - item.visitTime) < timespan * 24 * 60 * 60 * 1000;
   });
   return filteredItems;
 }
